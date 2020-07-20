2701124e4962311e478e9f25c09bae012d8d6f6b
Kunal Srivastav <kunal.srivastav_cs19@gla.ac.in>
2020-07-20 12:51:23 +0530

    Initial Commit

---
+++ src/lab/LabTask1.java
@@ -0,0 +1,16 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kunal Srivastav (kunalsrivastav)
+ *   Date: 20-07-2020
+ *   Time: 11:27
+ *   File: LabTask1.java
+ */
+
+package lab;
+
+// Task 1 - Create a "Hello, World!" program in Java.
+
+public class LabTask1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/lab/LabTask2.java
@@ -0,0 +1,31 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kunal Srivastav (kunalsrivastav)
+ *   Date: 20-07-2020
+ *   Time: 11:28
+ *   File: LabTask2.java
+ */
+
+package lab;
+
+// Task 2 - Create a program in Java to declare, initialize and use local variables of all the primitive data types in Java.
+
+public class LabTask2 {
+    public static void main(String[] args) {
+        byte byteNumberExample = 100;
+        short shortNumberExample = 1001;
+        int integerNumberExample = 6361;
+        long longNumberExample = 998865544;
+        float floatNumberExample = 0.3f;
+        double doubleNumberExample = 223.32435664;
+        char characterExample = 'K';
+        boolean booleanExample = true;
+        System.out.println("byteNumberExample = " + byteNumberExample);
+        System.out.println("shortNumber = " + shortNumberExample);
+        System.out.println("integerNumberExample = " + integerNumberExample);
+        System.out.println("longNumberExample = " + longNumberExample);
+        System.out.println("floatNumberExample = " + floatNumberExample);
+        System.out.println("doubleNumberExample = " + doubleNumberExample);
+        System.out.println("characterExample = " + characterExample);
+        System.out.println("booleanExample = " + booleanExample);
+    }
+}
